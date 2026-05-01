c17021563c0f75b47ec63ef81f18b593583dec45
Rakibislam22 <rakibislam13122018@gmail.com>
2026-05-01 23:55:16 +0600

    feat: Add navbar and hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import bgImg from '../../public/bg-imj.png';
+
+const Hero = () => {
+    return (
+        <div className='bg-[url()] bg-cover bg-center h-64 w-full'>
+            
+        </div>
+    );
+};
+
+export default Hero;
